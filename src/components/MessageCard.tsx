ff473c32e3bd505534af4432c2710e4363243f93
kykt35 <kykt35@gmail.com>
2019-12-15 16:44:52 +0900

    Create MessageCard

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import { Card, makeStyles } from '@material-ui/core';
+
+
+interface MessageCardProps {
+  children?:string;
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '85%',
+        padding: 10,
+        fontSize: '16px',
+        borderRadius: 8,
+        whiteSpace: 'pre-line',
+    },
+  })
+)
+
+const MessageCard: FC<MessageCardProps> = ({
+  children="テキスト"
+}) => {
+  const classes = useStyles()
+  return (
+    <Card className={classes.root}>
+      {children}
+    </Card>
+  );
+}
+
+export default MessageCard;
