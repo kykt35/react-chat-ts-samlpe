5f3ab052ec3101f6b573d32713b048421d44e2b1
kykt35 <kykt35@gmail.com>
2019-12-15 16:43:14 +0900

    Create AppHeader

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+import {  Box, makeStyles } from '@material-ui/core';
+
+
+const AppHeader:FC = () => {
+
+  return (
+    <Box>
+      header
+    </Box>
+  );
+}
+
+export default AppHeader
+
+;
