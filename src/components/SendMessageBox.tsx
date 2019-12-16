ab0ab0ab65ba2b07fe15ce3671795c761f402a02
kykt35 <kykt35@gmail.com>
2019-12-16 14:51:37 +0900

    Create sendMessageBox

--- src/components/SendMessageBox.tsx
+++ src/components/SendMessageBox.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useState } from 'react';
-import { Button, Box, TextareaAutosize, makeStyles } from '@material-ui/core';
+import { Button, Box, TextareaAutosize, Input, makeStyles } from '@material-ui/core';
 import {  BoxProps } from '@material-ui/core/Box';
 import {  ButtonProps } from '@material-ui/core/Button';
 import classNames from 'classnames';
 
 interface SendMessageBoxProps {
-  onSendMessage?: (text: string)=>void;
+  onSendMessage?: (text: string, username: string)=>void;
   className?: BoxProps["className"]
+  onChangeUsename?: (username: string)=>void;
 
 }
 
@@ -27,26 +28,43 @@ const useStyles = makeStyles(theme => ({
     },
     textarea: {
       width: "100%"
+    },
+    username: {
+      textAlign: "left",
+      display: "block"
     }
   })
 )
 
-const SendMessageBox: FC<SendMessageBoxProps> = ({onSendMessage,  className}) => {
+const SendMessageBox: FC<SendMessageBoxProps> = ({onSendMessage,  className, onChangeUsename}) => {
 
   const [text, setText] = useState('');
+  const [username, setUsername] = useState('');
   const handleTextFieldChange = (e:  React.ChangeEvent<HTMLTextAreaElement>)=>{
     setText(e.target.value)
   }
+  const handleNameFieldChange = (e:  React.ChangeEvent<HTMLTextAreaElement>)=>{
+    setUsername(e.target.value)
+
+    if (onChangeUsename) {
+      onChangeUsename(e.target.value)
+    }
+  }
   const handleClick = (e: React.MouseEvent<HTMLElement>)=> {
     e.preventDefault()
-    if (onSendMessage) { onSendMessage(text) }
+    if (!text || !username) {return }
+
+    if (onSendMessage) { onSendMessage(text, username) }
     setText('');
   }
   const classes = useStyles();
   return (
     <Box className={classNames(classes.sendBox, className)}>
+      <Input  value={username} className={classes.username} onChange={handleNameFieldChange} placeholder="なまえ"/>
       <Box className={classes.inner}>
         <TextareaAutosize rows={2} className={classes.textarea} value={text} onChange={handleTextFieldChange}/>
+
+
         <Button variant="contained" color="primary" onClick={handleClick}>send</Button>
       </Box>
     </Box>
