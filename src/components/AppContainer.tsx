514d35474247c7fa602d1314672ce1d817931708
kykt35 <kykt35@gmail.com>
2019-12-16 14:52:29 +0900

    Create AppContainer

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Container, Box, TextField, makeStyles } from '@material-ui/core';
 import MessageArea from './MessageArea'
 import SendMessageBox from './SendMessageBox';
-import AppHeader from './MessageArea'
+import AppHeader from './AppHeader'
+import firebase, { db } from '../firebase'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,21 +22,64 @@ const useStyles = makeStyles(theme => ({
   })
 )
 
+interface messsageProps {
+  username:string;
+  text:string;
+}
 
 const AppContainer: FC = () => {
-  const [values, setValues] = useState([{username: "", text: "", isMe: false}]);
-  const messages =[{username: "1234", text: "aaaa"},{username: "2345", text: "いいいい", isMe: true},]
+  const [user, setUser] = useState({username: ""})
+  const [messages, setMessages] = useState([{username: "", text: ""}]);
   const classes = useStyles()
-  const sendMessage = (text: string) =>{
-  let val = values.slice();
-  val.push({username: "1234", text: text, isMe: true})
-  setValues(val)
+
+useEffect(()=>{
+  let storeMessages = [{username: "", text: ""}]
+  db.collection("messages").orderBy('createdAt', 'asc').get().then((querySnapshot) => {
+
+      querySnapshot.forEach((doc) => {
+          storeMessages.push({username: doc.data().username, text: doc.data().text})
+      });
+  setMessages(storeMessages)
+  });
+
+})
+
+const postMessageToFirestore = (values: messsageProps) => {
+  db.collection("messages").add({
+      username: values.username,
+      text: values.text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+
+}
+
+
+  const sendMessage = (text: string, username: string) =>{
+    let message = {username: username, text: text,  }
+    postMessageToFirestore(message)
+
+    let newMessages=messages.slice()
+    newMessages.push(message)
+
+    setMessages(newMessages)
   }
+
+  const handleChangeUsername = (username: string) => {
+    setUser({username: username})
+  }
+
   return (
     <Container maxWidth="sm" className={classes.root}>
-
-      <MessageArea chatMessages={values}/>
-      <SendMessageBox className={classes.sendBox} onSendMessage={sendMessage}/>
+      <AppHeader/>
+      <MessageArea chatMessages={messages} user={user}/>
+      <SendMessageBox className={classes.sendBox} onSendMessage={sendMessage} onChangeUsename={handleChangeUsername}/>
     </Container>
   );
 }
